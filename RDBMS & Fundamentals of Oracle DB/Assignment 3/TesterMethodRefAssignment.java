50d44d568e805e3a80b771b61ef972444cc8cc7a
Swairik <swairikdey1001@gmail.com>
2023-02-04 19:49:25 +0530

    updated assignment 3

--- RDBMS & Fundamentals of Oracle DB/Assignment 3/TesterMethodRefAssignment.java
+++ RDBMS & Fundamentals of Oracle DB/Assignment 3/TesterMethodRefAssignment.java
@@ -1,15 +1,11 @@
-package day9;
-
 @FunctionalInterface
 interface MethodRefInterface {
 	Person getPerson(String name, int age);
 }
 
-public class MethodRefAssignment {
+public class TesterMethodRefAssignment {
 	public static void main(String[] args) {
-		MethodRefInterface obj = Person :: new;
+		MethodRefInterface obj = Person::new;
 		System.out.println(obj.getPerson("Peter", 25));
-
 	}
-
 }
